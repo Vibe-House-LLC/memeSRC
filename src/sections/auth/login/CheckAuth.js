621d30ddc6136e90d2a4fde486cce6b02083e967
Austin Woodard <noxileproduction@gmail.com>
2022-12-08 00:24:47 -0500

    Add loading screen while waiting for auth check

--- src/sections/auth/login/CheckAuth.js
+++ src/sections/auth/login/CheckAuth.js
@@ -1,3 +1,4 @@
+import { Backdrop, CircularProgress } from "@mui/material";
 import { Auth } from "aws-amplify";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -17,7 +18,8 @@ import { useNavigate } from "react-router-dom";
 
 export default function CheckAuth(props) {
     const navigate = useNavigate();
-    const [isSignedIn, setSignedIn] = useState();
+    const backdrop = true;
+    const [isSignedIn, setSignedIn] = useState(null);
     
     useEffect(() => {
         Auth.currentAuthenticatedUser().then(() =>
@@ -27,6 +29,19 @@ export default function CheckAuth(props) {
             setSignedIn(false)
         });
     });
+
+    // I'm not really sure if we want to use this. It pops up just long enough to be annoying. 
+    // That being said, it's nice to know that this is also a built in component in MUI.
+    if (isSignedIn === null) {
+        return (
+            <Backdrop
+                sx={{ color: '#fff', zIndex: "3000" }}
+                open={backdrop}
+            >
+                <CircularProgress color="inherit" />
+            </Backdrop>
+        )
+    }
 
     if (isSignedIn === true) {
         return props.children;
