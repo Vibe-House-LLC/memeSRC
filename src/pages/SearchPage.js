7b8d3f58156ac94e4e0a9d51080c573cf6c2321f
Davis Engeler <davisengeler+github@gmail.com>
2022-12-18 15:28:09 -0500

    Fix code formatting

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -79,53 +79,53 @@ function SearchPage() {
 
   return (
     <div>
-  <form onSubmit={handleSearch}>
-    <label htmlFor="search-term">Search Term:
-      <input
-        type="text"
-        id="search-term"
-        value={searchTerm}
-        onChange={e => setSearchTerm(e.target.value)}
-      />
-    </label>
-    <label htmlFor="series-title">
-      Series Title:
-      <input
-        type="text"
-        id="series-title"
-        value={seriesTitle}
-        onChange={e => setSeriesTitle(e.target.value)}
-      />
-    </label>
-    <button type="submit">Search</button>
-  </form>
-  <br /><br />
-  <Grid container spacing={2} className={classes.root}>
-    {loading ? (
-      <CircularProgress />
-    ) : results && results.map(result => (
-      <Grid item xs={12} sm={6} md={4} key={result.fid}>
-        <div className={classes.thumbnailContainer}>
-          <Card>
-            <CardMedia
-              className={classes.thumbnailImage}
-              component="img"
-              src={`https://memesrc.com${result.frame_image}`}
-              alt={result.subtitle}
-              title={result.subtitle}
-            />
-            <Typography className={classes.info} variant="body2">
-              Subtitle: {result.subtitle}<br />
-              Series Name: {result.series_name}<br />
-              Season: {result.season_number}<br />
-              Episode: {result.episode_number}
-            </Typography>
-          </Card>
-        </div>
+      <form onSubmit={handleSearch}>
+        <label htmlFor="search-term">Search Term:
+          <input
+            type="text"
+            id="search-term"
+            value={searchTerm}
+            onChange={e => setSearchTerm(e.target.value)}
+          />
+        </label>
+        <label htmlFor="series-title">
+          Series Title:
+          <input
+            type="text"
+            id="series-title"
+            value={seriesTitle}
+            onChange={e => setSeriesTitle(e.target.value)}
+          />
+        </label>
+        <button type="submit">Search</button>
+      </form>
+      <br /><br />
+      <Grid container spacing={2} className={classes.root}>
+        {loading ? (
+          <CircularProgress />
+        ) : results && results.map(result => (
+          <Grid item xs={12} sm={6} md={4} key={result.fid}>
+            <div className={classes.thumbnailContainer}>
+              <Card>
+                <CardMedia
+                  className={classes.thumbnailImage}
+                  component="img"
+                  src={`https://memesrc.com${result.frame_image}`}
+                  alt={result.subtitle}
+                  title={result.subtitle}
+                />
+                <Typography className={classes.info} variant="body2">
+                  Subtitle: {result.subtitle}<br />
+                  Series Name: {result.series_name}<br />
+                  Season: {result.season_number}<br />
+                  Episode: {result.episode_number}
+                </Typography>
+              </Card>
+            </div>
+          </Grid>
+        ))}
       </Grid>
-    ))}
-  </Grid>
-</div>
+    </div>
   );
 }
 
