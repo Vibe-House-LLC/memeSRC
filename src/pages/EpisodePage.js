ebb7b7cf5bed0e41009bae4e8fbc572cafbef74a
Austin Woodard <noxileproduction@gmail.com>
2023-01-21 15:13:56 -0500

    Add load more functionality

--- src/pages/EpisodePage.js
+++ src/pages/EpisodePage.js
@@ -1,7 +1,9 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 import { useParams } from 'react-router-dom';
-import { Grid, CircularProgress, Card } from '@mui/material';
+import { Grid, CircularProgress, Card, Button } from '@mui/material';
 import styled from '@emotion/styled';
+import { Stack } from '@mui/system';
+import { LoadingButton } from '@mui/lab';
 // import FullScreenSearch from '../sections/search/FullScreenSearch';
 // import TopBannerSearch from '../sections/search/TopBannerSearch';
 
@@ -24,10 +26,7 @@ const StyledCard = styled(Card)`
 
 const StyledCardMedia = styled.img`
   width: 100%;
-  height: 300px;
-  aspect-ratio: '16/9';
-  object-fit: contain;
-  object-position: center;
+  height: auto;
   background-color: black;
 `;
 
@@ -40,9 +39,10 @@ const StyledTypography = styled.p(({ theme }) => ({
 export default function EpisodePage() {
   // const [searchTerm, setSearchTerm] = useState('');
   // const [seriesTitle, setSeriesTitle] = useState('');
-  const [results, setResults] = useState(null);
+  const [results, setResults] = useState([]);
   const [sessionID, setSessionID] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [loadingMore, setLoadingMore] = useState(false);
 
   const memoizedResults = useMemo(() => results, [results]);
 
@@ -64,25 +64,29 @@ export default function EpisodePage() {
     }
   }, [])
 
- useEffect(() => {
+  const loadFrames = useCallback((start) => {
     if (sessionID) {
-      setLoading(true);
-      const apiEpisodeLookupUrl = `https://api.memesrc.com/?series=${seriesId}&season=${seasonNum}&episode=${episodeNum}&start=0&sessionID=${sessionID}`
-
+      const apiEpisodeLookupUrl = `https://api.memesrc.com/?series=${seriesId}&season=${seasonNum}&episode=${episodeNum}&start=${start}&sessionID=${sessionID}`
+      setLoadingMore(true);
       fetch(apiEpisodeLookupUrl)
         .then(response => response.json())
         .then(data => {
-          setResults(data);
+          setResults([...results, ...data]);
           setLoading(false);
+          setLoadingMore(false);
         })
         .catch(error => {
           console.error(error);
           setLoading(false);
+          setLoadingMore(false);
         });
     } else {
-      setLoading(true);
       console.log('Session ID not ready yet.')
     }
+  }, [sessionID, seriesId, seasonNum, episodeNum, results]);
+
+  useEffect(() => {
+    loadFrames('0');
   }, [sessionID, seriesId, seasonNum, episodeNum]);
 
 
@@ -93,29 +97,36 @@ export default function EpisodePage() {
     <>
       {/* {!memoizedResults && !loading && <FullScreenSearch searchFunction={handleSearch} setSearchTerm={setSearchTerm} setSeriesTitle={setSeriesTitle} searchTerm={searchTerm} seriesTitle={seriesTitle} />}
       {(memoizedResults || loading) && <TopBannerSearch searchFunction={handleSearch} setSearchTerm={setSearchTerm} setSeriesTitle={setSeriesTitle} searchTerm={searchTerm} seriesTitle={seriesTitle} />} */}
-      <Grid container spacing={2} marginTop={5}>
-        {loading ? (
-          <StyledCircularProgress />
-        ) : memoizedResults && memoizedResults.map(result => (
-          <Grid item xs={12} sm={6} md={4} key={result.fid}>
-            <a href={`/editor/${result.fid}`} style={{ textDecoration: 'none' }}>
-              <StyledCard>
-                <StyledCardMedia
-                  component="img"
-                  src={`https://memesrc.com${result.frame_image}`}
-                  alt={result.subtitle}
-                  title={result.subtitle} />
-                <StyledTypography variant="body2">
-                  Subtitle: {result.subtitle}<br />
-                  Series: {result.series_name}<br />
-                  Season: {result.season_number}<br />
-                  Episode: {result.episode_number}
-                </StyledTypography>
-              </StyledCard>
-            </a>
+      <Stack spacing={2} direction='column' padding={3}>
+        <Grid container spacing={2} marginTop={5}>
+          {loading ? (
+            <StyledCircularProgress />
+          ) : memoizedResults && memoizedResults.map(result => (
+            <Grid item xs={12/2} sm={12/2} md={12/5} key={result.fid}>
+              <a href={`/editor/${result.fid}`} style={{ textDecoration: 'none' }}>
+                <StyledCard>
+                  <StyledCardMedia
+                    component="img"
+                    src={`https://memesrc.com${result.frame_image}`}
+                    alt={result.subtitle}
+                    title={result.subtitle} />
+                  <StyledTypography variant="body2">
+                    Subtitle: {result.subtitle}<br />
+                    Series: {result.series_name}<br />
+                    Season: {result.season_number}<br />
+                    Episode: {result.episode_number}
+                  </StyledTypography>
+                </StyledCard>
+              </a>
+            </Grid>
+          ))}
+        </Grid>
+        <Grid container justifyContent='center'>
+          <Grid item xs={12} sm={3} md={1}>
+            {!loading && <LoadingButton loading={loadingMore} variant='contained' fullWidth onClick={() => loadFrames(results.length)}>Load More</LoadingButton>}
           </Grid>
-        ))}
-      </Grid>
+        </Grid>
+      </Stack>
     </>
   );
 }
