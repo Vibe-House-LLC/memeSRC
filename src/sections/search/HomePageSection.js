30dbcb4147df27dc32efc6fc43973c74e497868b
Austin Woodard <noxileproduction@gmail.com>
2023-01-23 21:51:39 -0500

    Change button size

--- src/sections/search/HomePageSection.js
+++ src/sections/search/HomePageSection.js
@@ -29,27 +29,31 @@ HomePageSection.propTypes = {
 
 };
 
-export default function HomePageSection({backgroundColor, textColor, title, subtitle, buttons, bottomImage, buttonSubtext}) {
+export default function HomePageSection({ backgroundColor, textColor, title, subtitle, buttons, bottomImage, buttonSubtext }) {
 
 
   return (
     <StyledGridContainer container justifyItems='center' paddingX={3} backgroundColor={backgroundColor}>
       <Grid item xs={12} textAlign='center' marginY='auto'>
-        <Typography component='h2' variant='h2' sx={{color: textColor}} marginBottom={4}>
+        <Typography component='h2' variant='h2' sx={{ color: textColor }} marginBottom={4}>
           {title}
         </Typography>
-        <Typography component='h4' variant='h4' sx={{color: textColor}} marginBottom={4}>
+        <Typography component='h4' variant='h4' sx={{ color: textColor }} marginBottom={4}>
           {subtitle}
         </Typography>
-        {buttons.map((button, index) => (
-            <Button key={index} startIcon={<Favorite />} href={button.destination} variant="contained" sx={{margin: '10px'}}>
-              {button.title}
-            </Button>
+        <Grid container justifyContent='center' spacing={2}>
+          {buttons.map((button, index) => (
+            <Grid item xs={12} sm='auto'>
+              <Button key={index} startIcon={<Favorite />} href={button.destination} variant="contained" size="large" fullWidth={{xs: true, sm: false}}>
+                {button.title}
+              </Button>
+            </Grid>
           )
-        )}
+          )}
+        </Grid>
         <br />
-        <Button href={buttonSubtext.href} startIcon={buttonSubtext.emoji} sx={{marginTop: '12px'}}>
-          <Typography sx={{textDecoration: 'underline', fontSize: '.95em', fontWeight: '800', color: textColor}}>
+        <Button href={buttonSubtext.href} startIcon={buttonSubtext.emoji} sx={{ marginTop: '12px' }}>
+          <Typography sx={{ textDecoration: 'underline', fontSize: '.95em', fontWeight: '800', color: textColor }}>
             {buttonSubtext.text}
           </Typography>
         </Button>
@@ -59,7 +63,7 @@ export default function HomePageSection({backgroundColor, textColor, title, subt
           maxHeight: bottomImage.maxHeight || '400px',
           height: 'auto',
           width: '100%'
-        }}/>
+        }} />
       </Grid>
     </StyledGridContainer>
   )
