998c07d40a77956b0039bbcabc3a00cc230add39
Davis Engeler <davisengeler@users.noreply.github.com>
2023-02-17 13:58:21 -0500

    Add error page

---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,51 @@
+import { Helmet } from 'react-helmet-async';
+import { Link as RouterLink } from 'react-router-dom';
+// @mui
+import { styled } from '@mui/material/styles';
+import { Button, Typography, Container, Box } from '@mui/material';
+
+// ----------------------------------------------------------------------
+
+const StyledContent = styled('div')(({ theme }) => ({
+  maxWidth: 480,
+  margin: 'auto',
+  minHeight: '100vh',
+  display: 'flex',
+  justifyContent: 'center',
+  flexDirection: 'column',
+  padding: theme.spacing(12, 0),
+}));
+
+// ----------------------------------------------------------------------
+
+export default function ErrorPage() {
+  return (
+    <>
+      <Helmet>
+        <title> Oops | memeSRC 2.0 </title>
+      </Helmet>
+
+      <Container>
+        <StyledContent sx={{ textAlign: 'center', alignItems: 'center' }}>
+          <Typography variant="h3" paragraph>
+            Oops, there was a problem!
+          </Typography>
+
+          <Typography sx={{ color: 'text.secondary' }}>
+            Sorry, it seems like memeSRC is struggling right now. Try again in a sec.
+          </Typography>
+
+          <Box
+            component="img"
+            src="/assets/illustrations/illustration_404.svg"
+            sx={{ height: 260, mx: 'auto', my: { xs: 5, sm: 10 } }}
+          />
+
+          <Button to="/" size="large" variant="contained" component={RouterLink}>
+            Go to Home
+          </Button>
+        </StyledContent>
+      </Container>
+    </>
+  );
+}
