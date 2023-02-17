16ac36f46ba79d22ff382b91e008468678ebcee6
Davis Engeler <davisengeler@users.noreply.github.com>
2023-02-16 23:10:24 -0500

    Update instructional text formatting

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -632,9 +632,9 @@ const EditorPage = ({ setSeriesTitle, shows }) => {
                             {imageUploading && <center><CircularProgress sx={{ margin: '30%' }} /></center>}
                         </DialogContentText>
                     </DialogContent>
-                    <DialogContentText sx={{ paddingX: 4, marginY: 'auto'}}>
+                    <DialogContentText sx={{ paddingX: 4, marginTop: 'auto', paddingBottom: 2}}>
                         <center>
-                            <p>☝️ {('ontouchstart' in window) ? 'Tap and hold' : 'Right click'} the image to save, or use a quick action:</p>
+                            <p>☝️ <b>{('ontouchstart' in window) ? 'Tap and hold' : 'Right click'} the image to save</b>, or use a quick action:</p>
                         </center>
                     </DialogContentText>
                     <DialogActions sx={{ marginBottom: 'auto', display: 'inline-flex', padding: '0 23px' }}>
