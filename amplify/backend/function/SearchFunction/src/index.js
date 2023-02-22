a7026f8eee424b34524ccedebe2781c934f05f4f
Davis Engeler <davisengeler@users.noreply.github.com>
2023-02-21 20:28:50 -0500

    Update amplify beta env

--- amplify/backend/function/SearchFunction/src/index.js
+++ amplify/backend/function/SearchFunction/src/index.js
@@ -1,3 +1,17 @@
+/*
+Use the following code to retrieve configured secrets from SSM:
+
+const aws = require('aws-sdk');
+
+const { Parameters } = await (new aws.SSM())
+  .getParameters({
+    Names: ["opensearch_user","opensearch_pass","opensearch_url"].map(secretName => process.env[secretName]),
+    WithDecryption: true,
+  })
+  .promise();
+
+Parameters will be of the form { Name: 'secretName', Value: 'secretValue', ... }[]
+*/
 /* Amplify Params - DO NOT EDIT
   ENV
   REGION
