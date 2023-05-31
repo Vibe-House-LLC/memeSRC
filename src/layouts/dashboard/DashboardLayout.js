246a11915ae0c39e08b105b41b8c4551191bdde5
Austin Woodard <noxileproduction@gmail.com>
2023-05-31 19:32:49 -0400

    Fix padding and margins

--- src/layouts/dashboard/DashboardLayout.js
+++ src/layouts/dashboard/DashboardLayout.js
@@ -8,32 +8,34 @@ import Nav from './nav';
 
 // ----------------------------------------------------------------------
 
-const APP_BAR_MOBILE = 45;
-const APP_BAR_DESKTOP = 45;
 
-const StyledRoot = styled('div')({
-  display: 'flex',
-  minHeight: '100%',
-  overflow: 'hidden',
-});
-
-const Main = styled('div')(({ theme }) => ({
-  flexGrow: 1,
-  overflow: 'auto',
-  minHeight: '100%',
-  // paddingTop: APP_BAR_MOBILE + 24,
-  // paddingBottom: theme.spacing(10),
-  // [theme.breakpoints.up('lg')]: {
-  //   paddingTop: APP_BAR_DESKTOP + 24,
-  //   paddingLeft: theme.spacing(2),
-  //   paddingRight: theme.spacing(2),
-  // },
-}));
 
 // ----------------------------------------------------------------------
 
 export default function DashboardLayout() {
   const [open, setOpen] = useState(false);
+
+  const APP_BAR_MOBILE = window.location.pathname === '/' ? 0 : 45;
+  const APP_BAR_DESKTOP = window.location.pathname === '/' ? 0 : 45;
+
+  const StyledRoot = styled('div')({
+    display: 'flex',
+    minHeight: '100%',
+    overflow: 'hidden',
+  });
+
+  const Main = styled('div')(({ theme }) => ({
+    flexGrow: 1,
+    overflow: 'auto',
+    minHeight: '100%',
+    paddingTop: APP_BAR_MOBILE,
+    paddingBottom: window.location.pathname === '/' ? 0 : theme.spacing(10),
+    [theme.breakpoints.up('lg')]: {
+      paddingTop: APP_BAR_DESKTOP,
+      // paddingLeft: theme.spacing(2),
+      // paddingRight: theme.spacing(2),
+    },
+  }));
 
   return (
     <StyledRoot>
