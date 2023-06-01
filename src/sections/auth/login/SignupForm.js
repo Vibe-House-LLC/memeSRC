7ce35f2dca6e793746e0e1edc3874ce36a648076
Austin Woodard <noxileproduction@gmail.com>
2023-05-31 22:25:23 -0400

    Add form validation to signup page

--- src/sections/auth/login/SignupForm.js
+++ src/sections/auth/login/SignupForm.js
@@ -29,55 +29,140 @@ export default function SignupForm(props) {
 
   const [password, setPassword] = useState('');
 
-  const createUser = async () => {
-    setSignupStatus({
-      'loading': true,
-      'disabled': true,
-      'text': 'Signing Up...'
-    })
+  const [confirmPassword, setConfirmPassword] = useState('');
 
-    Auth.signUp({
-      username,
-      password,
-      attributes: {
-        email,          // optional
-        // other custom attributes
-      },
-      autoSignIn: { // optional - enables auto sign in after user is confirmed
-        enabled: true,
-      }
-    }).then((result) => {
-      setSignupStatus({
-        'loading': false,
-        'disabled': true,
-        'text': 'Sign Up Complete'
-      });
-      console.log(result)
-      props.setUser(result.user)
-      console.log(result);
-    }).catch((err) => {
-      setSignupStatus({
-        'loading': false,
-        'disabled': false,
-        'error': err,
-        'text': 'Sign Up'
-      });
-      alert(err);
+  const [formErrors, setFormErrors] = useState({
+    username: false,
+    email: false,
+    password: false,
+    confirmPassword: false,
+    passwordMismatch: false,
+    passwordLength: false
+  });
+
+  const checkForErrors = () => {
+    let _username;
+    let _email;
+    let _password;
+    let _confirmPassword;
+    const _passwordMismatch = (password !== confirmPassword)
+    let _passwordLength;
+
+    if (!username) {
+      _username = true
+    }
+    if (!password) {
+      _password = true
+    }
+    if (!confirmPassword) {
+      _confirmPassword = true
+    }
+    if (!email) {
+      _email = true
+    }
+
+    if (password.length < 8) {
+      _passwordLength = true
+    }
+
+    setFormErrors({
+      username: _username,
+      email: _email,
+      password: _password,
+      confirmPassword: _confirmPassword,
+      passwordMismatch: _passwordMismatch,
+      passwordLength: _passwordLength,
     });
+
+    return (_username || _email || _password || _confirmPassword || _passwordMismatch || _passwordLength)
+  }
+
+
+  const createUser = async () => {
+    const errors = checkForErrors();
+
+    if (errors) {
+      setSignupStatus({
+        'loading': true,
+        'disabled': true,
+        'text': 'Signing Up...'
+      })
+
+      Auth.signUp({
+        username,
+        password,
+        attributes: {
+          email,          // optional
+          // other custom attributes
+        },
+        autoSignIn: { // optional - enables auto sign in after user is confirmed
+          enabled: true,
+        }
+      }).then((result) => {
+        setSignupStatus({
+          'loading': false,
+          'disabled': true,
+          'text': 'Sign Up Complete'
+        });
+        console.log(result)
+        props.setUser(result.user)
+        console.log(result);
+      }).catch((err) => {
+        setSignupStatus({
+          'loading': false,
+          'disabled': false,
+          'error': err,
+          'text': 'Sign Up'
+        });
+        console.log(err);
+      });
+    }
   }
 
   return (
     <>
       <Stack spacing={3}>
-        <TextField name="username" label="Username" onInput={(x) => setUsername(x.target.value)} />
+        <TextField
+          name="username"
+          label="Username"
+          error={formErrors.username}
+          onChange={(x) => {
+            setUsername(x.target.value)
+            setFormErrors({
+              ...formErrors,
+              username: false
+            })
+          }}
+        />
 
-        <TextField name="email" label="Email address" onInput={(x) => setEmail(x.target.value)} />
+        <TextField
+          name="email"
+          label="Email address"
+          error={formErrors.email}
+          onChange={(x) => {
+            setEmail(x.target.value)
+            setFormErrors({
+              ...formErrors,
+              email: false
+            })
+          }}
+        />
 
         <TextField
           name="password"
           label="Password"
+          error={formErrors.password || formErrors.passwordMismatch}
           type={showPassword ? 'text' : 'password'}
-          onInput={(x) => setPassword(x.target.value)}
+          onChange={(x) => {
+            setPassword(x.target.value)
+            setFormErrors({
+              ...formErrors,
+              password: false,
+              passwordMismatch: false,
+              passwordLength: false
+            })
+          }}
+          helperText={formErrors.password ? '' : `${formErrors.passwordLength ? 'Password is not long enough' : ''}${(formErrors.passwordLength && formErrors.passwordMismatch) ? ' & ' : ''}${formErrors.passwordMismatch ? 'Passwords do not match' : ''}`}
           InputProps={{
             endAdornment: (
               <InputAdornment position="end">
@@ -88,16 +173,32 @@ export default function SignupForm(props) {
             ),
           }}
         />
+        <TextField
+          name="confirm-password"
+          label="Confirm Password"
+          error={formErrors.confirmPassword || formErrors.passwordMismatch}
+          type={showPassword ? 'text' : 'password'}
+          onChange={(x) => {
+            setConfirmPassword(x.target.value)
+            setFormErrors({
+              ...formErrors,
+              confirmPassword: false,
+              passwordMismatch: false,
+              passwordLength: false
+            })
+          }}
+          helperText={formErrors.confirmPassword ? '' : `${formErrors.passwordLength ? 'Password is not long enough' : ''}${(formErrors.passwordLength && formErrors.passwordMismatch) ? ' & ' : ''}${formErrors.passwordMismatch ? 'Passwords do not match' : ''}`}
+        />
       </Stack>
 
-      <Stack direction="row" alignItems="center" justifyContent="space-between" sx={{ my: 2 }}>
+      {/* <Stack direction="row" alignItems="center" justifyContent="space-between" sx={{ my: 2 }}>
         <Checkbox name="remember" label="Remember me" />
         <Link variant="subtitle2" underline="hover">
           Forgot password?
         </Link>
-      </Stack>
+      </Stack> */}
 
-      <LoadingButton fullWidth size="large" type="submit" variant="contained" loading={signupStatus.loading} onClick={createUser} id="signup-btn" disabled={signupStatus.disabled}>
+      <LoadingButton sx={{ my: 3 }} fullWidth size="large" type="submit" variant="contained" loading={signupStatus.loading} onClick={createUser} id="signup-btn" disabled={signupStatus.disabled}>
         {signupStatus.text}
       </LoadingButton>
     </>
