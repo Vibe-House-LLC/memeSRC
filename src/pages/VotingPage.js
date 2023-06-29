64783cab2b3ce034a964db8dcdec2211347fd27a
Austin Woodard <noxileproduction@gmail.com>
2023-06-29 17:47:00 -0400

    Add loading spinner and signed out user handling

--- src/pages/VotingPage.js
+++ src/pages/VotingPage.js
@@ -3,16 +3,18 @@ import { API, graphqlOperation } from 'aws-amplify';
 import { Container, Grid, Card, CardContent, Typography, IconButton, CircularProgress, Box } from '@mui/material';
 import { ArrowUpward, ArrowDownward } from '@mui/icons-material';
 import FlipMove from 'react-flip-move';
-
+import { useLocation, useNavigate } from 'react-router-dom';
 import { listSeries } from '../graphql/queries';
 import { UserContext } from '../UserContext';
 
 export default function VotingPage() {
+    const navigate = useNavigate();
     const [shows, setShows] = useState([]);
     const [votes, setVotes] = useState({});
     const [loading, setLoading] = useState(true);
     const [votingStatus, setVotingStatus] = useState({});
     const [userVotes, setUserVotes] = useState({});
+    const location = useLocation();
 
     const { user, setUser } = useContext(UserContext)
 
@@ -26,7 +28,7 @@ export default function VotingPage() {
             const seriesData = await API.graphql({
                 ...graphqlOperation(listSeries),
                 authMode: "API_KEY"
-              })
+            })
             const voteData = await API.get('publicapi', '/vote/list');
             const sortedShows = seriesData.data.listSeries.items.sort((a, b) => (
                 voteData.votes[b.id] || 0
@@ -42,8 +44,8 @@ export default function VotingPage() {
 
     const handleVote = async (idx, boost) => {
         const seriesId = shows[idx].id;
-        setVotingStatus((prevStatus) => ({ ...prevStatus, [seriesId]: true }));
-    
+        setVotingStatus((prevStatus) => ({ ...prevStatus, [seriesId]: boost }));
+
         try {
             const result = await API.post('publicapi', '/vote', {
                 body: {
@@ -51,21 +53,21 @@ export default function VotingPage() {
                     boost
                 }
             })
-    
+
             setUserVotes(prevUserVotes => ({ ...prevUserVotes, [seriesId]: boost }));
-    
+
             setVotes(prevVotes => {
                 const newVotes = { ...prevVotes };
                 newVotes[seriesId] = (newVotes[seriesId] || 0) + boost;
-    
+
                 const sortedShows = [...shows].sort((a, b) => (
                     newVotes[b.id] || 0
                 ) - (newVotes[a.id] || 0));
                 setShows(sortedShows);
-    
+
                 return newVotes;
             });
-    
+
             setVotingStatus((prevStatus) => ({ ...prevStatus, [seriesId]: false }));
         } catch (error) {
             setVotingStatus((prevStatus) => ({ ...prevStatus, [seriesId]: false }));
@@ -73,7 +75,7 @@ export default function VotingPage() {
             console.log(error.response)
         }
     };
-    
+
     const handleUpvote = (idx) => {
         handleVote(idx, 1);
     };
@@ -117,15 +119,23 @@ export default function VotingPage() {
                                     <CardContent>
                                         <Box display="flex" alignItems="center">
                                             <Box mr={2}>
-                                                <IconButton aria-label="upvote" onClick={() => handleUpvote(idx)} disabled={userVotes[show.id] || votingStatus[show.id]}>
-                                                    <ArrowUpward sx={{ color: userVotes[show.id] === 1 ? 'success.main' : 'inherit' }} />
-                                                </IconButton>
+                                                {votingStatus[show.id] === 1 ?
+                                                    <CircularProgress size={20} sx={{ ml: 1.2, mb: 1.5 }} />
+                                                    :
+                                                    <IconButton aria-label="upvote" onClick={() => user ? handleUpvote(idx) : navigate(`/login?dest=${encodeURIComponent(location.pathname)}`)} disabled={userVotes[show.id] || votingStatus[show.id]}>
+                                                        <ArrowUpward sx={{ color: userVotes[show.id] === 1 ? 'success.main' : 'inherit' }} />
+                                                    </IconButton>
+                                                }
                                                 <Typography variant="subtitle1" gutterBottom textAlign='center'>
                                                     {votes[show.id] || 0}
                                                 </Typography>
-                                                <IconButton aria-label="downvote" onClick={() => handleDownvote(idx)} disabled={userVotes[show.id] || votingStatus[show.id]}>
-                                                    <ArrowDownward sx={{ color: userVotes[show.id] === -1 ? 'error.main' : 'inherit' }} />
-                                                </IconButton>
+                                                {votingStatus[show.id] === -1 ?
+                                                    <CircularProgress size={20} sx={{ ml: 1.2, mt: 1.5 }} />
+                                                    :
+                                                    <IconButton aria-label="downvote" onClick={() => user ? handleDownvote(idx) : navigate(`/login?dest=${encodeURIComponent(location.pathname)}`)} disabled={userVotes[show.id] || votingStatus[show.id]}>
+                                                        <ArrowDownward sx={{ color: userVotes[show.id] === -1 ? 'error.main' : 'inherit' }} />
+                                                    </IconButton>
+                                                }
                                             </Box>
                                             <Box flexGrow={1}>
                                                 <Box display="flex" alignItems="center">
