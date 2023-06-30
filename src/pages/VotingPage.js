3b4fb18323bd18b89db1a901805d35382a5607cf
Davis Engeler <davisengeler@users.noreply.github.com>
2023-06-30 14:41:26 -0400

    Add placeholder up and downvote count display

--- src/pages/VotingPage.js
+++ src/pages/VotingPage.js
@@ -27,6 +27,9 @@ export default function VotingPage() {
   const [votingStatus, setVotingStatus] = useState({});
   const [userVotes, setUserVotes] = useState({});
   const [searchText, setSearchText] = useState('');
+  const [upvotes, setUpvotes] = useState({});
+  const [downvotes, setDownvotes] = useState({});
+
   const location = useLocation();
 
   const { user, setUser } = useContext(UserContext);
@@ -49,6 +52,13 @@ export default function VotingPage() {
       setShows(sortedShows);
       setVotes(voteData.votes);
       setUserVotes(voteData.userVotes);
+
+      // TODO: Use placeholders for upvotes and downvotes for now
+      const placeholderUpvotes = seriesData.data.listSeries.items.reduce((obj, item) => ({ ...obj, [item.id]: 420 }), {});
+      const placeholderDownvotes = seriesData.data.listSeries.items.reduce((obj, item) => ({ ...obj, [item.id]: 69 }), {});
+
+      setUpvotes(placeholderUpvotes);
+      setDownvotes(placeholderDownvotes);
     } catch (error) {
       console.error('Error fetching series data:', error);
     }
@@ -116,131 +126,124 @@ export default function VotingPage() {
     .filter((show) => show.statusText === 'requested')
     .filter((show) => show.name.toLowerCase().includes(searchText.toLowerCase()));
 
-  return (
-    <Container maxWidth="md">
-      <Box my={4}>
-        <Typography variant="h3" component="h1" gutterBottom>
-          Vote for New Shows
-        </Typography>
-        <Typography variant="subtitle2">
-          Help prioritize requests by voting on your favorite shows. Upvote the shows you want to see more, and downvote
-          the shows you're not interested in.
-        </Typography>
-      </Box>
-      <Box my={2}>
-        <TextField
-          fullWidth
-          variant="outlined"
-          value={searchText}
-          onChange={handleSearchChange}
-          placeholder="Search requested shows..."
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <IconButton>
-                  <Search />
-                </IconButton>
-              </InputAdornment>
-            ),
-            endAdornment: (
-              <InputAdornment position="end">
-                <IconButton edge="end" onClick={() => setSearchText('')} disabled={!searchText}>
-                  <Close />
-                </IconButton>
-              </InputAdornment>
-            ),
-          }}
-        />
-      </Box>
-      <Grid container style={{ minWidth: '100%' }}>
-        {loading ? (
-          <CircularProgress />
-        ) : (
-          <FlipMove style={{ minWidth: '100%' }}>
-            {filteredShows.map((show, idx) => (
-              <Grid item xs={12} key={show.id} style={{ marginBottom: 15 }}>
-                <Card>
-                  <CardContent>
-                    <Box display="flex" alignItems="center">
-                      <Box mr={2}>
-                        {votingStatus[show.id] === 1 ? (
-                          <CircularProgress size={20} sx={{ ml: 1.2, mb: 1.5 }} />
-                        ) : (
-                          <IconButton
-                            aria-label="upvote"
-                            onClick={() =>
-                              user
-                                ? handleUpvote(show.id) 
-                                : navigate(`/login?dest=${encodeURIComponent(location.pathname)}`)
-                            }
-                            disabled={userVotes[show.id] || votingStatus[show.id]}
-                          >
-                            <ArrowUpward sx={{ color: userVotes[show.id] === 1 ? 'success.main' : 'inherit' }} />
-                          </IconButton>
-                        )}
-                        <Typography variant="subtitle1" gutterBottom textAlign="center">
-                          {votes[show.id] || 0}
-                        </Typography>
-                        {votingStatus[show.id] === -1 ? (
-                          <CircularProgress size={20} sx={{ ml: 1.2, mt: 1.5 }} />
-                        ) : (
-                          <IconButton
-                            aria-label="downvote"
-                            onClick={() =>
-                              user
-                                ? handleDownvote(show.id) 
-                                : navigate(`/login?dest=${encodeURIComponent(location.pathname)}`)
-                            }
-                            disabled={userVotes[show.id] || votingStatus[show.id]}
-                          >
-                            <ArrowDownward sx={{ color: userVotes[show.id] === -1 ? 'error.main' : 'inherit' }} />
-                          </IconButton>
-                        )}
-                      </Box>
-                      <Box flexGrow={1}>
-                        <Box display="flex" alignItems="center">
-                          <Box mr={2}>
-                            <img src={show.image} alt={show.name} style={showImageStyle} />
+    return (
+      <Container maxWidth="md">
+        <Box my={4}>
+          <Typography variant="h3" component="h1" gutterBottom>
+            Vote for New Shows
+          </Typography>
+          <Typography variant="subtitle2">
+            Help prioritize requests by voting on your favorite shows. Upvote the shows you want to see more, and downvote
+            the shows you're not interested in.
+          </Typography>
+        </Box>
+        <Box my={2}>
+          <TextField
+            fullWidth
+            variant="outlined"
+            value={searchText}
+            onChange={handleSearchChange}
+            placeholder="Search requested shows..."
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <IconButton>
+                    <Search />
+                  </IconButton>
+                </InputAdornment>
+              ),
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton edge="end" onClick={() => setSearchText('')} disabled={!searchText}>
+                    <Close />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Box>
+        <Grid container style={{ minWidth: '100%' }}>
+          {loading ? (
+            <CircularProgress />
+          ) : (
+            <FlipMove style={{ minWidth: '100%' }}>
+              {filteredShows.map((show, idx) => (
+                <Grid item xs={12} key={show.id} style={{ marginBottom: 15 }}>
+                  <Card>
+                    <CardContent>
+                      <Box display="flex" alignItems="center">
+                        <Box mr={2}>
+                          <Box>
+                            {votingStatus[show.id] === 1 ? (
+                              <CircularProgress size={20} sx={{ ml: 1.2, mb: 1.5 }} />
+                            ) : (
+                              <IconButton
+                                aria-label="upvote"
+                                onClick={() =>
+                                  user
+                                    ? handleUpvote(show.id)
+                                    : navigate(`/login?dest=${encodeURIComponent(location.pathname)}`)
+                                }
+                                disabled={userVotes[show.id] || votingStatus[show.id]}
+                              >
+                                <ArrowUpward sx={{ color: userVotes[show.id] === 1 ? 'success.main' : 'inherit' }} />
+                              </IconButton>
+                            )}
                           </Box>
                           <Box>
-                            <Typography variant="h4">{show.name}</Typography>
-                            <Typography variant="subtitle2">{show.year}</Typography>
-                            <Typography variant="body2" color="text.secondary" mt={1} style={descriptionStyle}>
-                              {show.description}
+                            <Typography variant="h5" gutterBottom textAlign="center" paddingTop={0.5}>
+                              {votes[show.id] || 0}
                             </Typography>
                           </Box>
-                        </Box>
-                      </Box>
-                    </Box>
-                  </CardContent>
-                </Card>
-              </Grid>
-            ))}
-            <Grid
-              item
-              xs={12}
-              style={{
-                marginTop: 45,
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                padding: '0 20px',
-              }}
-            >
-              <Typography variant="h6" gutterBottom>
-                Missing something?
-              </Typography>
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={() => window.open('https://forms.gle/8CETtVbwYoUmxqbi7', '_blank')}
-              >
-                Request a Show.
-              </Button>
-            </Grid>
-          </FlipMove>
-        )}
-      </Grid>
-    </Container>
-  );
+                          <Box>
+                            {votingStatus[show.id] === -1 ? (
+                              <CircularProgress size={20} sx={{ ml: 1.2, mt: 1.5 }} />
+                            ) : (
+                              <IconButton
+                                aria-label="downvote"
+                                onClick={() =>
+                                  user
+                                    ? handleDownvote(show.id)
+                                    : navigate(`/login?dest=${encodeURIComponent(location.pathname)}`)
+                                }
+                                disabled={userVotes[show.id] || votingStatus[show.id]}
+                              >
+                                <ArrowDownward sx={{ color: userVotes[show.id] === -1 ? 'error.main' : 'inherit' }} />
+                              </IconButton>
+                            )}
+                          </Box>
+                          </Box>
+          <Box flexGrow={1}>
+            <Box display="flex" alignItems="center">
+              <Box mr={2}>
+                <img src={show.image} alt={show.name} style={showImageStyle} />
+              </Box>
+              <Box>
+                <Typography variant="h4">{show.name}</Typography>
+                <Box display="flex" alignItems="center">
+                  <Typography variant="subtitle2" color="success.main">
+                    <ArrowUpward fontSize="small" sx={{ verticalAlign: 'middle' }} />
+                    {upvotes[show.id] || 0}
+                  </Typography>
+                  <Typography variant="subtitle2" color="error.main" ml={2}>
+                    <ArrowDownward fontSize="small" sx={{ verticalAlign: 'middle' }} />
+                    {downvotes[show.id] || 0}
+                  </Typography>
+                </Box>
+                <Typography variant="body2" color="text.secondary" mt={1} style={descriptionStyle}>
+                  {show.description}
+                </Typography>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+      </CardContent>
+    </Card>
+  </Grid>
+))}
+            </FlipMove>
+          )}
+        </Grid>
+      </Container>
+    );
 }
