2cee82f95790ed90858041f7c80bbd593cbf1407
Davis Engeler <davisengeler@users.noreply.github.com>
2023-08-06 14:42:03 -0400

    Add basic project list functionality

--- src/pages/EditorProjectsPage.js
+++ src/pages/EditorProjectsPage.js
@@ -1,16 +1,20 @@
-import { useEffect } from 'react';
-import { Helmet } from 'react-helmet-async';
-import { Container, Typography, Breadcrumbs, Link } from '@mui/material';
-import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import { useEffect, useState } from 'react';
+import { Container, Typography, Grid, Card, CardActionArea, CardContent, Box } from '@mui/material';
 import BasePage from './BasePage';
 
 export default function EditorProjectsPage() {
-  // useState declarations here
+  const [projects, setProjects] = useState([]);
 
   useEffect(() => {
-    // Any initial data loading operations can go here.
-
-    // API calls here
+    setProjects([
+      { id: 1, title: "Project 1", imageUrl: "https://picsum.photos/id/237/200/267", createdDate: "2023-01-01" },
+      { id: 2, title: "Project 2", imageUrl: "https://picsum.photos/id/238/200/267", createdDate: "2023-02-02" },
+      { id: 3, title: "Project 3", imageUrl: "https://picsum.photos/id/239/200/267", createdDate: "2023-03-03" },
+      { id: 4, title: "Project 4", imageUrl: "https://picsum.photos/id/240/200/267", createdDate: "2023-04-04" },
+      { id: 5, title: "Project 5", imageUrl: "https://picsum.photos/id/241/200/267", createdDate: "2023-05-05" },
+      { id: 6, title: "Project 6", imageUrl: "https://picsum.photos/id/242/200/267", createdDate: "2023-06-06" },
+      // add more projects as needed
+    ]);
   }, []);
 
   return (
@@ -22,7 +26,41 @@ export default function EditorProjectsPage() {
         { path: "/editor/new", name: "Projects" }
       ]}
     >
-      {/* Add your page-specific components or elements here */}
+      <Container sx={{ py: 5 }}>
+        <Grid container spacing={3}>
+          {projects.map((project) => (
+            <Grid item key={project.id} xs={12} sm={6} md={4}>
+              <Card>
+                <CardActionArea>
+                  <Box sx={{ pt: '75%' ,position: 'relative'}}>
+                    <img 
+                      src={project.imageUrl} 
+                      alt={project.title} 
+                      style={{ 
+                        position: 'absolute', 
+                        top: 0, 
+                        left: 0, 
+                        width: '100%', 
+                        height: '100%', 
+                        objectFit: 'cover', 
+                        objectPosition: 'center' 
+                      }} 
+                    />
+                  </Box>
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {project.title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Created on: {new Date(project.createdDate).toLocaleDateString()}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
     </BasePage>
   );
 }
