25c43a162f5cd8111f88842f13457d361b8e7c69
Davis Engeler <davisengeler@users.noreply.github.com>
2023-08-14 12:48:24 -0400

    Save aggregation results to analytics metrics

--- amplify/backend/function/memesrcAggregateVotes/src/index.js
+++ amplify/backend/function/memesrcAggregateVotes/src/index.js
@@ -1,15 +1,11 @@
 /* Amplify Params - DO NOT EDIT
+	API_MEMESRC_ANALYTICSMETRICSTABLE_ARN
+	API_MEMESRC_ANALYTICSMETRICSTABLE_NAME
 	API_MEMESRC_GRAPHQLAPIIDOUTPUT
 	API_MEMESRC_SERIESUSERVOTETABLE_ARN
 	API_MEMESRC_SERIESUSERVOTETABLE_NAME
 	ENV
 	REGION
-Amplify Params - DO NOT EDIT *//* Amplify Params - DO NOT EDIT
-    API_MEMESRC_GRAPHQLAPIIDOUTPUT
-    API_MEMESRC_SERIESUSERVOTETABLE_ARN
-    API_MEMESRC_SERIESUSERVOTETABLE_NAME
-    ENV
-    REGION
 Amplify Params - DO NOT EDIT */
 
 const AWS = require('aws-sdk');
@@ -21,7 +17,7 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient();
 exports.handler = async (event) => {
     console.log(`EVENT: ${JSON.stringify(event)}`);
 
-    // Scan the DynamoDB table
+    // Scan the SeriesUserVote table
     const params = {
         TableName: process.env.API_MEMESRC_SERIESUSERVOTETABLE_NAME
     };
@@ -32,7 +28,7 @@ exports.handler = async (event) => {
         console.error(`Error scanning DynamoDB table: ${JSON.stringify(scanError)}`);
         return {
             statusCode: 500,
-            body: JSON.stringify('Failed to scan DynamoDB table')
+            body: JSON.stringify('Failed to scan SeriesUserVote table')
         };
     }
 
@@ -51,6 +47,30 @@ exports.handler = async (event) => {
         } else if (boost < 0) {
             voteAggregation[seriesId].downvotes += Math.abs(boost);
         }
+    }
+
+    const currentTimestamp = new Date().toISOString();
+
+    // Write the aggregated data to the AnalyticsMetrics table
+    const analyticsMetricParams = {
+        TableName: process.env.API_MEMESRC_ANALYTICSMETRICSTABLE_NAME,
+        Item: {
+            id: `seriesVotes}`,  // This can be a UUID or any unique identifier
+            createdAt: currentTimestamp,
+            updatedAt: currentTimestamp,
+            value: JSON.stringify(voteAggregation),
+            __typename: "AnalyticsMetrics"
+        }
+    };
+
+    try {
+        await dynamoDb.put(analyticsMetricParams).promise();
+    } catch (error) {
+        console.error(`Error inserting into AnalyticsMetrics table: ${JSON.stringify(error)}`);
+        return {
+            statusCode: 500,
+            body: JSON.stringify('Failed to insert into AnalyticsMetrics table')
+        };
     }
 
     return {
