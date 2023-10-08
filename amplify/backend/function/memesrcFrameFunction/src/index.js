b3d4954c5b88300381107f55dad4f331af0d86d1
Davis Engeler <davisengeler@users.noreply.github.com>
2023-10-08 17:49:46 -0400

    Add frame subtitle type to graphql schema

--- amplify/backend/function/memesrcFrameFunction/src/index.js
+++ amplify/backend/function/memesrcFrameFunction/src/index.js
@@ -1,4 +1,11 @@
-/*
+/* Amplify Params - DO NOT EDIT
+	API_MEMESRC_FRAMESUBTITLETABLE_ARN
+	API_MEMESRC_FRAMESUBTITLETABLE_NAME
+	API_MEMESRC_GRAPHQLAPIIDOUTPUT
+	ENV
+	REGION
+	STORAGE_MEMESRCGENERATEDIMAGES_BUCKETNAME
+Amplify Params - DO NOT EDIT *//*
 Use the following code to retrieve configured secrets from SSM:
 
 const aws = require('aws-sdk');
