19da5ec681b2516ee9759f0de481bf84d97c27c5
Davis Engeler <davisengeler@users.noreply.github.com>
2023-10-13 15:35:56 -0400

    Fix nested links

--- src/layouts/dashboard/nav/index.js
+++ src/layouts/dashboard/nav/index.js
@@ -59,7 +59,6 @@ export default function Nav({ openNav, onCloseNav }) {
       </Box>
 
       <Box sx={{ mb: 5, mx: 2.5 }}>
-        <Link underline="none">
           {!userDetails.user &&
             <StyledAccount>
               <>
@@ -79,7 +78,6 @@ export default function Nav({ openNav, onCloseNav }) {
               </>
             </StyledAccount>
           }
-        </Link>
       </Box>
 
       <NavSection sx={{ paddingBottom: 8 }} data={navConfig} />
