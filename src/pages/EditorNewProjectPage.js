7c0fa609e8db77c65bea3fc6c11d5106347282b2
Davis Engeler <davisengeler@users.noreply.github.com>
2023-10-18 17:33:52 -0400

    Add search option navigation

--- src/pages/EditorNewProjectPage.js
+++ src/pages/EditorNewProjectPage.js
@@ -36,53 +36,54 @@ export default function EditorNewProjectPage() {
       ]}
     >
       <Grid container justifyContent="center" spacing={2} sx={{ mt: 4 }}>
-      <Grid item xs={12} sm={6} md={4} lg={3}>
-  <label htmlFor="upload-image" style={{ width: '100%', cursor: 'pointer' }}>
-    <Paper
-      elevation={6}
-      sx={{
-        p: 3,
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100%',
-      }}
-    >
-      <CloudUploadIcon sx={{ fontSize: 60, mb: 2 }} />
-      <Typography variant="h5" component="div" gutterBottom>
-        Upload Image
-      </Typography>
-      <Typography color="text.secondary">Choose your own image</Typography>
-    </Paper>
-    {/* Hidden input for the image upload */}
-    <Input
-      type="file"
-      id="upload-image"
-      style={{ display: 'none' }}
-      onChange={handleImageUpload}
-    />
-  </label>
-</Grid>
-
           <Grid item xs={12} sm={6} md={4} lg={3}>
-            <Paper
-              elevation={6}
-              sx={{
-                p: 3,
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '100%',
-              }}
-            >
-              <SearchIcon sx={{ fontSize: 60, mb: 2 }} />
-              <Typography variant="h5" component="div" gutterBottom>
-                Search Images
-              </Typography>
-              <Typography color="text.secondary">Find 35 million on memeSRC</Typography>
-            </Paper>
+            <label htmlFor="upload-image" style={{ width: '100%', cursor: 'pointer' }}>
+              <Paper
+                elevation={6}
+                sx={{
+                  p: 3,
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  height: '100%',
+                }}
+              >
+                <CloudUploadIcon sx={{ fontSize: 60, mb: 2 }} />
+                <Typography variant="h5" component="div" gutterBottom>
+                  Upload Image
+                </Typography>
+                <Typography color="text.secondary">Choose your own image</Typography>
+              </Paper>
+              {/* Hidden input for the image upload */}
+              <Input
+                type="file"
+                id="upload-image"
+                style={{ display: 'none' }}
+                onChange={handleImageUpload}
+              />
+            </label>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4} lg={3}>
+            <CardActionArea onClick={() => navigate('/')}>
+              <Paper
+                elevation={6}
+                sx={{
+                  p: 3,
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  height: '100%',
+                }}
+              >
+                <SearchIcon sx={{ fontSize: 60, mb: 2 }} />
+                <Typography variant="h5" component="div" gutterBottom>
+                  Search Images
+                </Typography>
+                <Typography color="text.secondary">Find 35 million on memeSRC</Typography>
+              </Paper>
+            </CardActionArea>
           </Grid>
 
           <Grid item xs={12} sm={6} md={4} lg={3}>
