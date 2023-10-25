585d903772888a74d1ab1dc3baf0fae58c6a53f1
Davis Engeler <davisengeler@users.noreply.github.com>
2023-10-25 15:54:35 -0400

    Improve projects list page

--- src/pages/EditorProjectsPage.js
+++ src/pages/EditorProjectsPage.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
-import { Container, Typography, Grid, Card, CardActionArea, CardContent, Box } from '@mui/material';
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import { Container, Typography, ImageList, ImageListItem, Button } from '@mui/material';
+import { Add } from '@mui/icons-material';
 import { useNavigate } from 'react-router-dom';
 import { API, Storage, graphqlOperation } from 'aws-amplify';
 import BasePage from './BasePage';
@@ -16,7 +16,6 @@ export default function EditorProjectsPage() {
         const projectsData = await API.graphql(graphqlOperation(listEditorProjects));
         const projectsList = projectsData.data.listEditorProjects.items;
         
-        // Map the fetched projects to the expected format with async operations
         const mappedProjectsPromises = projectsList.map(async project => {
           let imageUrl;
           const key = `projects/${project.id}-preview.jpg`;
@@ -37,8 +36,6 @@ export default function EditorProjectsPage() {
         });
   
         const mappedProjects = await Promise.all(mappedProjectsPromises);
-  
-        // Sort the projects by createdDate, with the newest first
         const sortedProjects = mappedProjects.sort((a, b) => new Date(b.createdDate) - new Date(a.createdDate));
         
         setProjects(sortedProjects);
@@ -51,58 +48,6 @@ export default function EditorProjectsPage() {
   }, []);
   
 
-  // Project Card sub-component
-  function ProjectCard({ project, isAddNew }) {
-    const onClick = isAddNew 
-    ? () => navigate('/editor/new')
-    : () => navigate(`/editor/project/${project.id}`);
-
-  return (
-    <Card>
-      <CardActionArea onClick={onClick}>
-        <Box
-          sx={{
-            height: '300px',
-            position: 'relative',
-            backgroundColor: isAddNew ? 'gray' : undefined,
-            display: isAddNew ? 'flex' : undefined,
-            justifyContent: isAddNew ? 'center' : undefined,
-            alignItems: isAddNew ? 'center' : undefined,
-          }}
-        >
-          {isAddNew ? (
-            <AddCircleOutlineIcon style={{ fontSize: '5rem' }} />
-          ) : (
-            <img
-              src={project.imageUrl}
-              alt={project.title}
-              style={{
-                position: 'absolute',
-                top: '50%',
-                left: '50%',
-                width: '100%',
-                height: '100%',
-                objectFit: 'contain', // changed from 'cover' to 'contain'
-                transform: 'translate(-50%, -50%)' // centers the image
-              }}
-            />
-          )}
-        </Box>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {isAddNew ? 'Create New Project' : project.title}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {isAddNew
-              ? 'Start a new editor project'
-              : `Created on: ${new Date(project.createdDate).toLocaleDateString()}`}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-    );
-  }
-
   return (
     <BasePage
       pageTitle="My Projects"
@@ -112,19 +57,31 @@ export default function EditorProjectsPage() {
         { path: '/editor/new', name: 'Projects' },
       ]}
     >
-      <Container sx={{ py: 5 }}>
-        <Grid container spacing={3}>
-          <Grid item key="addNew" xs={12} sm={6} md={4}>
-            <ProjectCard isAddNew />
-          </Grid>
-
+      <Container sx={{ padding: 0 }}>
+        <Button
+          fullWidth
+          variant="contained"
+          color="primary"
+          size="large"
+          onClick={() => navigate('/editor/new')}
+          sx={{ marginBottom: 2, backgroundColor: 'limegreen' }}
+          startIcon={<Add />}
+        >
+          New Project
+        </Button>
+        <ImageList variant="masonry" cols={3} gap={8}>
           {projects.map((project) => (
-            <Grid item key={project.id} xs={12} sm={6} md={4}>
-              <ProjectCard project={project} />
-            </Grid>
+            <ImageListItem key={project.id} onClick={() => navigate(`/editor/project/${project.id}`)}>
+              <img
+                src={project.imageUrl}
+                alt={project.title}
+                loading="lazy"
+              />
+            </ImageListItem>
           ))}
-        </Grid>
+        </ImageList>
       </Container>
     </BasePage>
   );
+  
 }
