effde35fcccecd3dc24093aa671712b6094af37a
Davis Engeler <davisengeler@users.noreply.github.com>
2024-02-18 11:15:29 -0500

    Add index processing progress bar

--- src/sections/server/CreateIndex.js
+++ src/sections/server/CreateIndex.js
@@ -1,13 +1,15 @@
 import React, { useState, useCallback, useEffect } from 'react';
-import { Box, Button, TextField } from '@mui/material';
+import { Box, Button, TextField, LinearProgress } from '@mui/material';
 
 function CreateIndex({ onProcessComplete }) {
     const [folderPath, setFolderPath] = useState('');
     const [isProcessing, setIsProcessing] = useState(false);
+    const [progress, setProgress] = useState(0);
     const [id, setId] = useState('');
 
     const handleProcessStart = useCallback(async () => {
         setIsProcessing(true);
+        setProgress(0); // Reset progress to 0
 
         try {
             const electron = window.require('electron');
@@ -19,6 +21,24 @@ function CreateIndex({ onProcessComplete }) {
             // Start the Python script without waiting for it to finish
             electron.ipcRenderer.send('start-python-script', ipcArguments);
             console.log('Python script execution started');
+
+            // Set up an interval to periodically check the processing status
+            const progressInterval = setInterval(async () => {
+                try {
+                    const response = await electron.ipcRenderer.invoke('fetch-processing-status', id);
+                    if (response.success && response.status) {
+                        const percentComplete = response.status.percent_complete;
+                        console.log("percentComplete", percentComplete)
+                        setProgress(Math.round(percentComplete));
+                        if (percentComplete >= 100) {
+                            clearInterval(progressInterval);
+                        }
+                    }
+                } catch (error) {
+                    console.error('Failed to fetch processing status:', error);
+                    clearInterval(progressInterval);
+                }
+            }, 5000); // Update progress every 5 seconds
         } catch (error) {
             console.error('Failed in starting processing steps:', error);
             setIsProcessing(false);
@@ -28,11 +48,6 @@ function CreateIndex({ onProcessComplete }) {
     useEffect(() => {
         const electron = window.require('electron');
 
-        // Listener for Python script started
-        const handleScriptStarted = (event, response) => {
-            console.log('Python script has started', response);
-        };
-
         // Listener for Python script response
         const handleScriptResponse = async (event, response) => {
             console.log('Python script executed with response:', response);
@@ -40,7 +55,7 @@ function CreateIndex({ onProcessComplete }) {
                 // Since the Python script finished, proceed with IPFS addition
                 try {
                     const processedFolderPath = `~/.memesrc/processing/${id}`; // Adjust the path as needed
-                    console.log(`About to add this to ipfs: ${processedFolderPath}`)
+                    console.log(`About to add this to ipfs: ${processedFolderPath}`);
                     const ipfsResult = await electron.ipcRenderer.invoke('add-processed-index-to-ipfs', processedFolderPath);
                     console.log('Added processed index to IPFS successfully:', ipfsResult);
 
@@ -55,16 +70,15 @@ function CreateIndex({ onProcessComplete }) {
                 console.error('Python script failed:', response.error);
             }
             setIsProcessing(false);
+            setProgress(0); // Reset progress
             setId(''); // Reset ID for the next use
             setFolderPath(''); // Reset folder path for the next use
         };
 
-        electron.ipcRenderer.on('python-script-started', handleScriptStarted);
         electron.ipcRenderer.on('python-script-response', handleScriptResponse);
 
         // Cleanup listeners on component unmount
         return () => {
-            electron.ipcRenderer.removeListener('python-script-started', handleScriptStarted);
             electron.ipcRenderer.removeListener('python-script-response', handleScriptResponse);
         };
     }, [id, onProcessComplete]);
@@ -97,7 +111,12 @@ function CreateIndex({ onProcessComplete }) {
                 >
                     Start Processing
                 </Button>
-                {isProcessing && <p>Processing...</p>}
+                {isProcessing && (
+                    <>
+                        <LinearProgress variant="determinate" value={Math.round(progress)} sx={{ mt: 2, mb: 1 }} />
+                        <p>Processing... {progress}%</p>
+                    </>
+                )}
             </Box>
         </>
     );
