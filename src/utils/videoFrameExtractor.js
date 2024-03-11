a61261d88aecf4285260b32af27da288afdd5c38
Davis Engeler <davisengeler@users.noreply.github.com>
2024-03-10 20:20:19 -0400

    Fix unnecessary fetch

--- src/utils/videoFrameExtractor.js
+++ src/utils/videoFrameExtractor.js
@@ -3,13 +3,7 @@ export async function extractVideoFrames(cid, season, episode, frameIndexes, fps
     return `https://api-dev.memesrc.com/dev/v2/frame/${cid}/${season}/${episode}/${frameId}`;
   });
 
-  const frameBlobs = await Promise.all(frameUrls.map(async (url) => {
-    const response = await fetch(url);
-    const blob = await response.blob();
-    return URL.createObjectURL(blob);
-  }));
-
-  return frameBlobs;
+  return frameUrls;
 }
 
 export async function extractFramesFromVideo(videoUrl, frameNumbers, assumedFps = 10, scaleFactor = 1.0) {
