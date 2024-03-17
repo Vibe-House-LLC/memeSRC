3a1fd62d61ec001598f78de773066f7e36e9ffe4
Davis Engeler <davisengeler@users.noreply.github.com>
2024-03-17 16:46:43 -0400

    Add binary content types to content api

---
+++ amplify/backend/api/memesrcContentApi/override.ts
@@ -0,0 +1,6 @@
+// This file is used to override the REST API resources configuration
+import { AmplifyApiRestResourceStackTemplate, AmplifyProjectInfo } from '@aws-amplify/cli-extensibility-helper';
+
+export function override(resources: AmplifyApiRestResourceStackTemplate, amplifyProjectInfo: AmplifyProjectInfo) {
+    resources.restApi.binaryMediaTypes = ["*/*"];
+}
