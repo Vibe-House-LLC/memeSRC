1afef581ef6a56d9654215252ce20664fea30387
Davis Engeler <davisengeler@users.noreply.github.com>
2024-03-26 15:57:43 -0400

    Fix episode page navigation

--- src/pages/V2EpisodePage.js
+++ src/pages/V2EpisodePage.js
@@ -1,7 +1,9 @@
+// V2EpisodePage.js
+
 import { Buffer } from "buffer";
 import React, { useState, useEffect, useContext } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
-import { CircularProgress, Container, Typography, Card, CardMedia, CardContent, Button, Grid, useMediaQuery } from '@mui/material';
+import { useParams } from 'react-router-dom';
+import { CircularProgress, Container, Typography, Card, CardMedia, CardContent, Button, Grid, useMediaQuery, Box } from '@mui/material';
 import PropTypes from 'prop-types';
 import { extractVideoFrames } from '../utils/videoFrameExtractor';
 import { UserContext } from '../UserContext';
@@ -11,13 +13,25 @@ V2EpisodePage.propTypes = {
   setSeriesTitle: PropTypes.func.isRequired,
 };
 
+const formatTimecode = (frameId, fps) => {
+  const totalSeconds = Math.floor(frameId / fps);
+  const hours = Math.floor(totalSeconds / 3600);
+  const minutes = Math.floor((totalSeconds % 3600) / 60);
+  const seconds = totalSeconds % 60;
+
+  return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+};
+
 export default function V2EpisodePage({ setSeriesTitle }) {
   const { user } = useContext(UserContext);
-  const navigate = useNavigate();
   const [results, setResults] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [loadingMore, setLoadingMore] = useState(false);
   const { cid, season, episode, frame } = useParams();
   const [confirmedCid, setConfirmedCid] = useState();
+  const [subtitles, setSubtitles] = useState([]);
+  const [lastPrev, setLastPrev] = useState(null);
+  const [lastNext, setLastNext] = useState(null);
   const fps = 10; // Frames per second
   const isMd = useMediaQuery(theme => theme.breakpoints.up('md'));
 
@@ -31,14 +45,12 @@ export default function V2EpisodePage({ setSeriesTitle }) {
 
   useEffect(() => {
     if (confirmedCid) {
-      const fetchSubtitlesAndFrames = async () => {
-        setLoading(true);
-
+      const fetchSubtitles = async () => {
         const subtitlesUrl = `https://memesrc.com/v2/${confirmedCid}/${season}/${episode}/_docs.csv`;
         const subtitlesResponse = await fetch(subtitlesUrl);
         const subtitlesCsv = await subtitlesResponse.text();
 
-        const subtitles = subtitlesCsv.split('\n').slice(1).map(line => {
+        const parsedSubtitles = subtitlesCsv.split('\n').slice(1).map(line => {
           const parts = line.split(',');
           return {
             season: parts[0],
@@ -50,10 +62,21 @@ export default function V2EpisodePage({ setSeriesTitle }) {
           };
         });
 
-        // Generate frame indexes to fetch based on fps stepping
+        setSubtitles(parsedSubtitles);
+      };
+
+      fetchSubtitles().catch(console.error);
+    }
+  }, [confirmedCid, season, episode]);
+
+  useEffect(() => {
+    if (confirmedCid && subtitles.length > 0) {
+      const fetchFrames = async () => {
+        setLoading(true);
+
         const frameIndexes = [];
-        for (let i = 0; i < fps * 60; i += fps) {
-          frameIndexes.push(parseInt(frame, 10) + i);
+        for (let i = 0; i < 60; i += 1) {
+          frameIndexes.push(parseInt(frame, 10) + i * fps);
         }
 
         const frames = await extractVideoFrames(confirmedCid, season, episode, frameIndexes, fps, 0.4);
@@ -65,67 +88,106 @@ export default function V2EpisodePage({ setSeriesTitle }) {
             fid: frameId.toString(),
             frame_image: frameUrl,
             subtitle: subtitle ? subtitle.subtitle_text : null,
+            timecode: formatTimecode(frameId, fps),
           };
         });
-        console.log("frameResults", frameResults)
 
         setResults(frameResults);
+        setLastPrev(frameResults[0].fid);
+        setLastNext(frameResults[frameResults.length - 1].fid);
         setLoading(false);
       };
 
-      fetchSubtitlesAndFrames().catch(console.error);
+      fetchFrames().catch(console.error);
     }
-  }, [confirmedCid, season, episode, frame, fps]);
+  }, [confirmedCid, season, episode, frame, fps, subtitles]);
 
-  const navigateFrames = (direction) => {
-    const currentFrame = parseInt(frame, 10);
-    const newFrame = direction === 'prev' ? Math.max(currentFrame - fps * 10, 0) : currentFrame + fps * 10;
-    navigate(`/episode/${cid}/${season}/${episode}/${newFrame}`);
+  const navigateFrames = async (direction) => {
+    setLoadingMore(true);
+  
+    const currentFrame = direction === 'prev' ? lastPrev : lastNext;
+    const newFrame = direction === 'prev'
+      ? Math.max(parseInt(currentFrame, 10) - fps * 15, 0)
+      : parseInt(currentFrame, 10) + fps;
+  
+    const frameIndexes = [];
+    const numFrames = direction === 'prev' ? 15 : 60;
+    for (let i = 0; i < numFrames; i += 1) {
+      frameIndexes.push(newFrame + i * fps);
+    }
+  
+    const frames = await extractVideoFrames(confirmedCid, season, episode, frameIndexes, fps, 0.4);
+  
+    const frameResults = frameIndexes.map((frameId, index) => {
+      const frameUrl = frames[index];
+      const subtitle = subtitles.find(sub => frameId >= sub.start_frame && frameId <= sub.end_frame);
+      return {
+        fid: frameId.toString(),
+        frame_image: frameUrl,
+        subtitle: subtitle ? subtitle.subtitle_text : null,
+        timecode: formatTimecode(frameId, fps),
+      };
+    });
+  
+    setResults(prevResults => {
+      const existingFids = new Set(prevResults.map(result => result.fid));
+      const newResults = frameResults.filter(result => !existingFids.has(result.fid));
+  
+      if (direction === 'prev') {
+        setLastPrev(newResults[0].fid);
+        return [...newResults, ...prevResults];
+      }
+  
+      setLastNext(newResults[newResults.length - 1].fid);
+      return [...prevResults, ...newResults];
+    });
+  
+    setLoadingMore(false);
   };
 
   return (
     <Container maxWidth="lg">
-      <Typography gutterBottom fontSize={isMd ? 24 : 15} component="div" style={{ marginTop: '20px' }}>
-        {cid} <br /><span style={{ fontSize: isMd ? 20 : 14 }}>Season {season}, Episode {episode}</span>
+      <Typography gutterBottom variant="h4" component="div" align="center" style={{ marginTop: '20px', marginBottom: '20px' }}>
+        {cid} <br /><span style={{ fontSize: '18px' }}>Season {season}, Episode {episode}</span>
       </Typography>
-      {parseInt(frame, 10) !== 0 && (
-        <Button disabled={loading} fullWidth={!isMd} variant="contained" onClick={() => navigateFrames('prev')} sx={{ mb: 4 }} style={{ marginTop: '20px' }}>
-          Previous Frames
+      <Box marginBottom="20px">
+        <Button fullWidth disabled={loading || loadingMore} variant="contained" onClick={() => navigateFrames('prev')} sx={{ mb: 2 }}>
+          {loadingMore ? 'Loading...' : 'Previous Frames'}
         </Button>
-      )}
+      </Box>
       {loading ? (
-        <center>
+        <Box display="flex" justifyContent="center" marginTop="50px">
           <CircularProgress />
-        </center>
+        </Box>
       ) : (
-        <Container maxWidth="md">
-          <Grid container spacing={2} alignContent="stretch">
-            {results.map((result) => (
-              <Grid item xs={12} key={result.fid}>
-                <div style={{ display: 'flex', justifyContent: 'center' }}>
-                  <Card component="a" href={`/frame/${cid}/${season}/${episode}/${result.fid}`} style={{ display: 'flex', textDecoration: 'none', width: '100%' }}>
-                    <CardMedia
-                      component="img"
-                      style={{ width: '50%', objectFit: 'contain' }}
-                      image={result.frame_image}
-                      alt={`Frame ${result.fid}`}
-                    />
-                    <CardContent sx={{ alignSelf: 'center' }}>
-                      <Typography variant="body1" color="textPrimary" component="p">
-                        {result.subtitle ? Buffer.from(result.subtitle, 'base64').toString() : 'No subtitle'}
-                      </Typography>
-                    </CardContent>
-                  </Card>
-                </div>
-              </Grid>
-            ))}
-          </Grid>
-        </Container>
+        <Grid container spacing={4}>
+          {results.map((result) => (
+            <Grid item xs={12} key={result.fid}>
+              <Card component="a" href={`/frame/${cid}/${season}/${episode}/${result.fid}`} style={{ display: 'flex', textDecoration: 'none', alignItems: 'center', padding: '20px' }}>
+                <CardMedia
+                  component="img"
+                  style={{ width: '30%', objectFit: 'contain', marginRight: '20px' }}
+                  image={result.frame_image}
+                  alt={`Frame ${result.fid}`}
+                />
+                <CardContent style={{ flex: 1 }}>
+                  <Typography variant="h6" color="textPrimary" component="p">
+                    {result.subtitle ? Buffer.from(result.subtitle, 'base64').toString() : '(...)'}
+                  </Typography>
+                  <Typography variant="subtitle1" color="textSecondary" component="p" style={{ marginTop: '10px' }}>
+                    Timecode: {result.timecode}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       )}
-      <br />
-      <Button disabled={loading} fullWidth={!isMd} variant="contained" onClick={() => navigateFrames('next')} sx={{ mt: 2 }} style={{ marginBottom: '20px' }}>
-        Next Frames
-      </Button>
+      <Box marginTop="20px">
+        <Button fullWidth disabled={loading || loadingMore} variant="contained" onClick={() => navigateFrames('next')}>
+          {loadingMore ? 'Loading...' : 'Next Frames'}
+        </Button>
+      </Box>
     </Container>
   );
 }
