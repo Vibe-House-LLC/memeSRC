ac0af0b2e6e371fa676c16590f2e07798e5671ad
Davis Engeler <davisengeler@users.noreply.github.com>
2024-03-29 14:00:27 -0400

    Adjust search page banner ad wrapper

--- src/ads/SearchPageBannerAd.js
+++ src/ads/SearchPageBannerAd.js
@@ -1,10 +1,20 @@
 import { Link, Typography, Box } from '@mui/material';
 import { useEffect } from 'react';
-import { Close, OpenInBrowser, Share } from '@mui/icons-material';
+import { Close } from '@mui/icons-material';
 import { useSubscribeDialog } from '../contexts/useSubscribeDialog';
 
 const SearchPageBannerAd = () => {
     const { openSubscriptionDialog } = useSubscribeDialog();
+
+    const adSnippet = (
+        <ins className="adsbygoogle"
+            style={{ display: 'block' }}
+            data-ad-format="auto"
+            data-ad-layout-key="-gw-3+1f-3d+2z"
+            data-ad-client="ca-pub-1307598869123774"
+            data-ad-slot="1685907131"
+        />
+    )
 
     useEffect(() => {
         // Load the adsbygoogle script
@@ -19,23 +29,24 @@ const SearchPageBannerAd = () => {
         window.adsbygoogle.push({});
     }, []);
 
+    const removeAdsLink = (
+        <Link onClick={(e) => { e.preventDefault(); openSubscriptionDialog(); }} sx={{ color: 'white', cursor: 'pointer' }} >
+            <Typography fontSize={14} textAlign='center' py={2} display='flex' alignItems='center'>
+                <Close fontSize='small' sx={{ mr: 0.5 }} /> <b>Remove ads w/ memeSRC Pro</b>
+            </Typography>
+        </Link>
+    );
+
     return (
-        <>
+        <Box sx={{ backgroundColor: 'black', borderRadius: 2, margin: 2 }}>
             <Box display='flex' justifyContent='center'>
-                <Link onClick={(e) => { e.preventDefault(); openSubscriptionDialog(); }} sx={{ color: 'white', cursor: 'pointer' }} >
-                    <Typography fontSize={14} textAlign='center' pb={2} display='flex' alignItems='center'>
-                        <Close fontSize='small' sx={{ mr: 0.5 }} /> <b>Remove ads w/ memeSRC Pro</b>
-                    </Typography>
-                </Link>
+                {removeAdsLink}
             </Box>
-            <ins className="adsbygoogle"
-                style={{ display: 'block' }}
-                data-ad-format="auto"
-                data-ad-layout-key="-gw-3+1f-3d+2z"
-                data-ad-client="ca-pub-1307598869123774"
-                data-ad-slot="1685907131"
-            />
-        </>
+                {adSnippet}
+            <Box display='flex' justifyContent='center'>
+                {removeAdsLink}
+            </Box>
+        </Box>
     );
 }
 
