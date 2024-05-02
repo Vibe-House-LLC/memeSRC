847242b71b44fd996663f7817fce65607b040fee
Austin Woodard <noxileproduction@gmail.com>
2024-05-01 22:21:18 -0400

    Revert auth pages and contexts to working state

--- src/sections/auth/login/GuestAuth.js
+++ src/sections/auth/login/GuestAuth.js
@@ -1,101 +1,69 @@
 import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
-import { API, Auth, Storage } from 'aws-amplify';
+import { useNavigate, useLocation } from "react-router-dom";
+import { API, Auth } from 'aws-amplify';
 import { PropTypes } from "prop-types";
 import { UserContext } from '../../../UserContext';
 
-const getProfilePicture = async (key) => {
-    const newProfilePicture = await Storage.get(key);
-    return newProfilePicture;
-};
-
 GuestAuth.propTypes = {
-    children: PropTypes.object
-};
+  children: PropTypes.object
+}
 
 export default function GuestAuth(props) {
-    const [user, setUser] = useState(null);
-    const location = useLocation();
+  const navigate = useNavigate();
+  const [content, setContent] = useState(null);
+  const [user, setUser] = useState(null);
+  const location = useLocation();
 
-    useEffect(() => {
-        const fetchUser = async () => {
-            try {
-                const authenticatedUser = await Auth.currentAuthenticatedUser();
-                const userDetails = await API.get('publicapi', '/user/get');
+  useEffect(() => {
+    const userDetails = window.localStorage.getItem('memeSRCUserDetails')
+    const userObject = { ...userDetails }
+    // console.log(userObject)
+    if (user && (user.userDetails !== userObject.userDetails)) {
+      // console.log('writing user')
+      // console.log(user)
+      window.localStorage.setItem('memeSRCUserDetails', JSON.stringify({ ...user?.signInUserSession?.accessToken?.payload, userDetails: { ...user.userDetails } }))
+      // console.log('New User Details')
+      // console.log({ ...user?.signInUserSession?.accessToken?.payload, ...user.userDetails })
+      // console.log('Full User Details');
+      // console.log(user)
 
-                let profilePhoto = null;
-                if (authenticatedUser?.attributes?.picture) {
-                    profilePhoto = await getProfilePicture(authenticatedUser.attributes.picture);
-                }
+    }
+  }, [user])
 
-                const updatedUser = {
-                    ...authenticatedUser,
-                    ...authenticatedUser.signInUserSession.accessToken.payload,
-                    userDetails: userDetails?.data?.getUserDetails,
-                    profilePhoto
-                };
 
-                setUser(updatedUser);
-                window.localStorage.setItem('memeSRCUserDetails', JSON.stringify(updatedUser));
-            } catch (error) {
-                console.log(error);
-                setUser(false);
-                window.localStorage.removeItem('memeSRCUserDetails');
-            }
-        };
+  useEffect(() => {
+    const localStorageUser = JSON.parse(window.localStorage.getItem('memeSRCUserDetails'))
+    if (localStorageUser) {
+      setUser(localStorageUser)
+    } else {
+      setUser(false)
+    }
+    // Set up the user context
+    // console.log(user)
+    Auth.currentAuthenticatedUser().then((x) => {
+      API.get('publicapi', '/user/get').then(userDetails => {
+        setUser({ ...x, ...x.signInUserSession.accessToken.payload, userDetails: userDetails?.data?.getUserDetails })  // if an authenticated user is found, set it into the context
+        // console.log(x)
+        window.localStorage.setItem('memeSRCUserDetails', JSON.stringify({ ...x.signInUserSession.accessToken.payload, userDetails: userDetails?.data?.getUserDetails }))
+        // console.log("Updating Amplify config to use AMAZON_COGNITO_USER_POOLS")
+        // Amplify.configure({
+        //     "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS",
+        // });
+      }).catch(err => console.log(err))
+    }).catch(() => {
+      setUser(false)  // indicate the context is ready but user is not auth'd
+      window.localStorage.removeItem('memeSRCUserInfo')
+      // console.log("There wasn't an authenticated user found")
+      // console.log("Updating Amplify config to use API_KEY")
+      // Amplify.configure({
+      //     "aws_appsync_authenticationType": "API_KEY",
+      // });
+    });
+  }, [location.pathname])
 
-        const cachedUserDetails = window.localStorage.getItem('memeSRCUserDetails');
-        if (cachedUserDetails) {
-            console.log(cachedUserDetails)
-            const parsedUserDetails = JSON.parse(cachedUserDetails);
-            setUser(parsedUserDetails);
-        }
-
-        fetchUser();
-    }, []);
-
-    const logout = () => {
-        return new Promise((resolve) => {
-            setUser(false);
-            window.localStorage.removeItem('memeSRCUserDetails');
-            resolve();
-        });
-    };
-
-    const login = (username, password) => {
-        return new Promise((resolve, reject) => {
-          Auth.signIn(username, password)
-            .then((user) => {
-              API.post('publicapi', '/user/update/status')
-                .then(async (response) => {
-                  const userDetails = await API.get('publicapi', '/user/get');
-                  let profilePhoto = null;
-                  if (user?.attributes?.picture) {
-                    profilePhoto = await getProfilePicture(user.attributes.picture);
-                  }
-                  const updatedUser = {
-                    ...user,
-                    ...user.signInUserSession.accessToken.payload,
-                    userDetails: userDetails?.data?.getUserDetails,
-                    profilePhoto,
-                  };
-                  setUser(updatedUser);
-                  window.localStorage.setItem('memeSRCUserDetails', JSON.stringify(updatedUser));
-                  resolve(updatedUser);
-                })
-                .catch((error) => {
-                  reject(error);
-                });
-            })
-            .catch((error) => {
-              reject(error);
-            });
-        });
-      };
-
-    return (
-        <UserContext.Provider value={{ user, setUser, logout, login }}>
-            {props.children}
-        </UserContext.Provider>
-    );
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {props.children}
+    </UserContext.Provider>
+  )
 }
