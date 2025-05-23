003e297ed2b1e2843839eab07af17b0ad9abf39a
Davis Engeler <davisengeler@users.noreply.github.com>
2025-05-23 11:18:27 -0400

    Disable unnecessary lint rules on collage export preview

--- src/components/collage/components/CollageResultDialog.js
+++ src/components/collage/components/CollageResultDialog.js
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React, { useCallback, useState } from 'react';
 import PropTypes from 'prop-types';
 import {
