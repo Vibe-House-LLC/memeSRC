7d5c9b063e66f23348eb33ccbfd2001c092adae4
davis <davisengeler@users.noreply.github.com>
2025-07-25 16:12:54 -0400

    Fix ESLint warnings in utility files

--- src/utils/videoFrameExtractor.js
+++ src/utils/videoFrameExtractor.js
@@ -1,12 +1,13 @@
 export async function extractVideoFrames(cid, season, episode, frameIndexes) {
   const frameUrls = frameIndexes.map(
-    (frameId) => `https://v2-${process.env.REACT_APP_USER_BRANCH}.memesrc.com/frame/${cid}/${season}/${episode}/${frameId}`
+    (frameId) =>
+      `https://v2-${process.env.REACT_APP_USER_BRANCH}.memesrc.com/frame/${cid}/${season}/${episode}/${frameId}`
   );
 
   return frameUrls;
 }
 
-export async function extractFramesFromVideo(_videoUrl, _frameNumbers, _assumedFps = 10, _scaleFactor = 1.0) {
+export async function extractFramesFromVideo() {
   console.warn('extractFramesFromVideo is deprecated. Please use extractVideoFrames instead.');
   return [];
 }
