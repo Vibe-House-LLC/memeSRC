d2c7da2fc6f71eba34323e1948264e81aaef6984
davis <davisengeler@users.noreply.github.com>
2025-07-26 01:07:23 -0400

    Fix StripeWatcher and allow dev origin

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    allowedDevOrigins: ["http://192.168.0.37:3000"],
+  },
 }
 
 module.exports = nextConfig
