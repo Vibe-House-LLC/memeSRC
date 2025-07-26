5889116fa6e547653561d02838b321ea4d35cf9e
davis <davisengeler@users.noreply.github.com>
2025-07-26 00:57:54 -0400

    Clean CRA remnants and set up Next.js

---
+++ pages/index.js
@@ -0,0 +1,31 @@
+import { Suspense } from 'react';
+import { Backdrop, CircularProgress } from '@mui/material';
+import { SearchDetailsProvider } from '../src/contexts/SearchDetailsProvider';
+import { CollageProvider } from '../src/contexts/CollageContext';
+import ThemeProvider from '../src/theme';
+import SnackBar from '../src/utils/Snackbar';
+import StripeWatcher from '../src/utils/StripeWatcher';
+
+export default function Home() {
+  return (
+    <ThemeProvider>
+      <SnackBar>
+        <StripeWatcher>
+          <Suspense
+            fallback={
+              <Backdrop open sx={{ backgroundColor: 'white' }}>
+                <CircularProgress />
+              </Backdrop>
+            }
+          >
+            <SearchDetailsProvider>
+              <CollageProvider>
+                <div>Home Page</div>
+              </CollageProvider>
+            </SearchDetailsProvider>
+          </Suspense>
+        </StripeWatcher>
+      </SnackBar>
+    </ThemeProvider>
+  );
+}
