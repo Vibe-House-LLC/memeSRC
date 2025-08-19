421fe5a3bc8e676191dd6077bde5a71d487706cd
Davis Engeler <davisengeler@users.noreply.github.com>
2025-08-18 20:38:25 -0400

    Convert fonts util to TypeScript

---
+++ src/utils/fonts.ts
@@ -0,0 +1,20 @@
+const fonts: string[] = [
+  "Arial",
+  "Courier New",
+  "Georgia",
+  "Verdana",
+  "Akbar",
+  "Baveuse",
+  "PULPY",
+  "scrubs",
+  "South Park",
+  "SPIDEY",
+  "HORROR",
+  "IMPACT",
+  "Star Jedi",
+  "twilight",
+  "zuume",
+];
+
+export default fonts;
+
