35c0ae1cdfe5e21a1549304163af9cfd93ba440d
Davis Engeler <davisengeler@users.noreply.github.com>
2025-08-18 20:09:27 -0400

    Convert color to hex util to typescript

---
+++ src/utils/colorToHex.ts
@@ -0,0 +1,23 @@
+const htmlColors: Record<string, string> = {
+  black: '#000000',
+  silver: '#c0c0c0',
+  gray: '#808080',
+  white: '#ffffff',
+  maroon: '#800000',
+  red: '#ff0000',
+  purple: '#800080',
+  fuchsia: '#ff00ff',
+  green: '#008000',
+  lime: '#00ff00',
+  olive: '#808000',
+  yellow: '#ffff00',
+  navy: '#000080',
+  blue: '#0000ff',
+  teal: '#008080',
+  aqua: '#00ffff',
+};
+
+export default function colorToHex(color: string): string {
+  return htmlColors?.[color] || color;
+}
+
